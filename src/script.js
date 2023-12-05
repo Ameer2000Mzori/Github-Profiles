d2602bbbf611fbdc63fe2c2a930c8dc6d935ebc6
Ameer <ameerapex496@gmail.com>
2023-12-05 11:32:43 +0100

    inserted data from api

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 // importing our elements
 var searchBtn = document.getElementsByClassName("search-Btn")[0];
 var inputText = document.getElementById("input-Text");
+var profileCard = document.getElementsByClassName("profile-Card")[0];
 // Import Axios
 var axios;
 // GitHub user profile Api
@@ -12,7 +13,9 @@ function fetchData(name) {
         .get(API_URL + name)
         .then(function (response) {
         // Handle the successful response
-        console.log("Axios Response:", response.data);
+        var data = response.data;
+        dynamicData(data);
+        console.log(data);
     })
         .catch(function (error) {
         // Handle errors
@@ -20,7 +23,9 @@ function fetchData(name) {
     });
 }
 // our functions
-var dynamicData = function () { };
+var dynamicData = function (data) {
+    profileCard.innerHTML = "\n\n<div class=\"prifle-Pic-Wrap\">\n  <img\n    class=\"profile-Img\"\n    src=\" ".concat(data.avatar_url, "\"\n    alt=\"\"\n  />\n</div>\n<div class=\"profile-Text-Wrap\">\n  <div class=\"name-And-Link-Wrap\">\n    <h1 class=\"name-Surename\">").concat(data.name, "</h1>\n    <a href=\"#\" target=\"_blank\">GO TO PROFILE</a>\n  </div>\n  <p class=\"bio-Text\">\n    ").concat(data.bio, "\n  </p>\n  <div class=\"repos-Followers-Wrap\">\n    <ul>\n      <li><p> ").concat(data.followers, " Followers</p></li>\n      <li><p> ").concat(data.following, " Following</p></li>\n      <li><p>").concat(data.public_repos, " Repos</p></li>\n    </ul>\n  </div>\n</div>\n    ");
+};
 // our event lisnters
 searchBtn.addEventListener("click", function () {
     var textVal = inputText.value;
@@ -44,3 +49,40 @@ addEventListener("keydown", function (e) {
 });
 // Call the function to fetch data
 fetchData("Ameer2000Mzori");
+// dynamic insert html tree
+// <div class="profile-Card">
+// <div class="prifle-Pic-Wrap">
+//   <img
+//     class="profile-Img"
+//     src=" https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+//     alt=""
+//   />
+// </div>
+// <div class="profile-Text-Wrap">
+//   <div class="name-And-Link-Wrap">
+//     <h1 class="name-Surename">Ameer Ameen</h1>
+//     <a href="#" target="_blank">GO TO PROFILE</a>
+//   </div>
+//   <p class="bio-Text">
+//     Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+//     placeat hic explicabo nisi magni, ipsa et molestias quae enim
+//     repudiandae temporibus molestiae? Exercitationem voluptatem,
+//     perspiciatis expedita fugiat doloremque consectetur ab? Lorem
+//     ipsum dolor sit amet consectetur adipisicing elit. Aut placeat hic
+//     explicabo nisi magni, ipsa et molestias quae enim repudiandae
+//     temporibus molestiae? Exercitationem voluptatem, perspiciatis
+//     expedita fugiat doloremque consectetur ab? Lorem ipsum dolor sit
+//     amet consectetur adipisicing elit. Aut placeat hic explicabo nisi
+//     magni, ipsa et molestias quae enim repudiandae temporibus
+//     molestiae? Exercitationem voluptatem, perspiciatis expedita fugiat
+//     doloremque consectetur ab?
+//   </p>
+//   <div class="repos-Followers-Wrap">
+//     <ul>
+//       <li><p>300 Followers</p></li>
+//       <li><p>15 Following</p></li>
+//       <li><p>10 Repos</p></li>
+//     </ul>
+//   </div>
+// </div>
+// </div>
--- src/script.ts
+++ src/script.ts
@@ -3,7 +3,9 @@ const searchBtn = document.getElementsByClassName(
   "search-Btn"
 )[0] as HTMLElement;
 const inputText = document.getElementById("input-Text") as any;
-
+const profileCard = document.getElementsByClassName(
+  "profile-Card"
+)[0] as HTMLElement;
 // Import Axios
 var axios;
 
@@ -17,7 +19,9 @@ function fetchData(name: any) {
     .get(API_URL + name)
     .then(function (response) {
       // Handle the successful response
-      console.log("Axios Response:", response.data);
+      let data: any = response.data;
+      dynamicData(data);
+      console.log(data);
     })
     .catch(function (error) {
       // Handle errors
@@ -26,7 +30,34 @@ function fetchData(name: any) {
 }
 
 // our functions
-const dynamicData = (): void => {};
+const dynamicData = (data): void => {
+  profileCard.innerHTML = `
+
+<div class="prifle-Pic-Wrap">
+  <img
+    class="profile-Img"
+    src=" ${data.avatar_url}"
+    alt=""
+  />
+</div>
+<div class="profile-Text-Wrap">
+  <div class="name-And-Link-Wrap">
+    <h1 class="name-Surename">${data.name}</h1>
+    <a href="#" target="_blank">GO TO PROFILE</a>
+  </div>
+  <p class="bio-Text">
+    ${data.bio}
+  </p>
+  <div class="repos-Followers-Wrap">
+    <ul>
+      <li><p> ${data.followers} Followers</p></li>
+      <li><p> ${data.following} Following</p></li>
+      <li><p>${data.public_repos} Repos</p></li>
+    </ul>
+  </div>
+</div>
+    `;
+};
 
 // our event lisnters
 
@@ -52,3 +83,42 @@ addEventListener("keydown", (e) => {
 
 // Call the function to fetch data
 fetchData(`Ameer2000Mzori`);
+
+// dynamic insert html tree
+
+// <div class="profile-Card">
+// <div class="prifle-Pic-Wrap">
+//   <img
+//     class="profile-Img"
+//     src=" https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+//     alt=""
+//   />
+// </div>
+// <div class="profile-Text-Wrap">
+//   <div class="name-And-Link-Wrap">
+//     <h1 class="name-Surename">Ameer Ameen</h1>
+//     <a href="#" target="_blank">GO TO PROFILE</a>
+//   </div>
+//   <p class="bio-Text">
+//     Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+//     placeat hic explicabo nisi magni, ipsa et molestias quae enim
+//     repudiandae temporibus molestiae? Exercitationem voluptatem,
+//     perspiciatis expedita fugiat doloremque consectetur ab? Lorem
+//     ipsum dolor sit amet consectetur adipisicing elit. Aut placeat hic
+//     explicabo nisi magni, ipsa et molestias quae enim repudiandae
+//     temporibus molestiae? Exercitationem voluptatem, perspiciatis
+//     expedita fugiat doloremque consectetur ab? Lorem ipsum dolor sit
+//     amet consectetur adipisicing elit. Aut placeat hic explicabo nisi
+//     magni, ipsa et molestias quae enim repudiandae temporibus
+//     molestiae? Exercitationem voluptatem, perspiciatis expedita fugiat
+//     doloremque consectetur ab?
+//   </p>
+//   <div class="repos-Followers-Wrap">
+//     <ul>
+//       <li><p>300 Followers</p></li>
+//       <li><p>15 Following</p></li>
+//       <li><p>10 Repos</p></li>
+//     </ul>
+//   </div>
+// </div>
+// </div>
