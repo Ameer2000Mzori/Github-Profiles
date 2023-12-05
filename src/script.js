2c02350e6c3f9bc2f68823e6b181600c27c8194d
Ameer <ameerapex496@gmail.com>
2023-12-05 11:14:05 +0100

    added axios to html tree

--- src/script.js
+++ src/script.js
@@ -1,1 +1,20 @@
-console.log("hallo world");
+// Import Axios
+var axios;
+// GitHub user profile URL
+var API_URL = "https://api.github.com/users/Ameer2000Mzori";
+// Fetching data with Axios
+function fetchData() {
+    // Make a GET request
+    axios
+        .get(API_URL)
+        .then(function (response) {
+        // Handle the successful response
+        console.log("Axios Response:", response.data);
+    })
+        .catch(function (error) {
+        // Handle errors
+        console.error("Axios Error:", error);
+    });
+}
+// Call the function to fetch data
+fetchData();
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,22 @@
-console.log("hallo world");
+// Import Axios
+let axios;
+// GitHub user profile URL
+const API_URL = "https://api.github.com/users/Ameer2000Mzori";
+
+// Fetching data with Axios
+function fetchData() {
+  // Make a GET request
+  axios
+    .get(API_URL)
+    .then((response) => {
+      // Handle the successful response
+      console.log("Axios Response:", response.data);
+    })
+    .catch((error) => {
+      // Handle errors
+      console.error("Axios Error:", error);
+    });
+}
+
+// Call the function to fetch data
+fetchData();
